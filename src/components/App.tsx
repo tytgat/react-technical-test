9e83ad0a190a02cad26226037f43be9e815e27c9
Alexandre Paixao <alexandre.paixao@stadline.com>
2023-06-26 13:04:45 +0200

    Add main features

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,39 @@
 import PokemonList from "./PokemonList";
 
+function Footer() {
+  return (
+    <table style={{ marginTop: "2em" }}>
+      <tr>
+        <td>
+          <a href="https://pokenode-ts.vercel.app/">Doc API Pokemon</a>
+        </td>
+        <td>
+          <a href="https://www.xplortechnologies.com/fr/travailler-chez-nous">Xplor Technologies</a>
+        </td>
+      </tr>
+      <tr>
+        <td colSpan={999}>
+          © 1998 Pokémon.{" "}
+          <a href="https://t.ly/uBmFu" rel="copyright">
+            ©
+          </a>{" "}
+          1995–2023 Nintendo/Creatures Inc./GAME FREAK inc. Pokémon
+        </td>
+      </tr>
+    </table>
+  );
+}
+
 function App() {
   return (
     <>
-      <h1>Bienvenue sur le site des amis des pokemons</h1>
-      <h5 className="blink">Yay !</h5>
+      <img src="src/assets/logo.png" width="800" />
+      <h1 className="rainbow">Bienvenue sur le site des amis des pokemons</h1>
+      <h5 className="blink">⭐ Yay ! ⭐</h5>
       <PokemonList />
+      <br />
+      <img src="src/assets/pokedex.png" />
+      <Footer />
     </>
   );
 }
--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,20 +1,45 @@
-import { PokemonSpecies } from "pokenode-ts";
+import { Pokemon, PokemonSpecies } from "pokenode-ts";
 import usePokeApi, { getLocalizedName, resolveResources } from "src/hooks/usePokeApi";
 
+interface PokemonProps {
+  species: PokemonSpecies;
+}
+
+function Pokemon({ species }: PokemonProps) {
+  const { data: pokemon } = usePokeApi(async (api) => {
+    const defaultVariety = species.varieties.find((variety) => variety.is_default);
+    if (defaultVariety) {
+      return api.utility.getResourceByUrl<Pokemon>(defaultVariety.pokemon.url);
+    }
+  });
+
+  return (
+    <tr>
+      <td width="1">
+        <img
+          src={pokemon?.sprites.other?.["official-artwork"].front_default ?? "src/assets/pokeball.png"}
+          style={{
+            height: "3em",
+          }}
+        />
+      </td>
+      <td>{getLocalizedName(species)}</td>
+    </tr>
+  );
+}
+
 function PokemonList() {
-  const { data: pokemons } = usePokeApi((api) =>
+  const { data: pokemonSpecies } = usePokeApi((api) =>
     api.pokemon.listPokemonSpecies(0, 10).then(resolveResources<PokemonSpecies>)
   );
 
-  if (!pokemons) return <div>Chargement ...</div>;
+  if (!pokemonSpecies) return <div>Chargement ...</div>;
 
   return (
-    <table border={1}>
+    <table border={1} style={{ background: "white", color: "blue", width: 800 }}>
       <tbody>
-        {pokemons.results.map((pokemon) => (
-          <tr key={pokemon.id}>
-            <td>{getLocalizedName(pokemon)}</td>
-          </tr>
+        {pokemonSpecies.results.map((s) => (
+          <Pokemon key={s.id} species={s} />
         ))}
       </tbody>
     </table>
